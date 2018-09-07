83e55a90a03c478374174d78d27e6476c5dca996
Algimantas Skuodis <algimantas.skuodis@visma.com>
2018-09-07 10:05:51 +0300

    Add project files.

---
+++ PrimeFactorsKata1/PrimeFactors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactorsKata1
+{
+    public class PrimeFactors
+    {
+        public static List<int> Generate(int number)
+        {
+            List<int> factors = new List<int>();
+            if (number == 1) return factors;
+
+            int x = 2;
+            while ( x <= number)
+            {
+                if (number % x == 0)
+                {
+                    factors.Add(x);
+                    number = number / x;
+                    x = 2;
+                } else
+                {
+                    x++;
+                }
+            }
+
+            return factors;
+        }
+    }
+}
---
+++ PrimeFactorsKata1/PrimeFactorsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace PrimeFactorsKata1
+{
+    [TestFixture]
+    public class PrimeFactorsTest
+    {
+        [Test]
+        [TestCase(1, new int[] { })]
+        [TestCase(2, new int[] { 2 })]
+        [TestCase(3, new int[] { 3 })]
+        [TestCase(4, new int[] { 2,2 })]
+        [TestCase(5, new int[] { 5 })]
+        [TestCase(6, new int[] { 2, 3 })]
+        [TestCase(7, new int[] { 7 })]
+        [TestCase(8, new int[] { 2, 2, 2 })]
+        [TestCase(9, new int[] { 3, 3 })]
+        [TestCase(10, new int[] { 2, 5 })]
+        [TestCase(12, new int[] { 2, 2, 3 })]
+        [TestCase(128, new int[] { 2, 2, 2, 2, 2, 2, 2 })]
+        public void TestWithSomethingReturnsResult(int number, int[] expected)
+        {
+            List<int> result = PrimeFactors.Generate( number );
+            Assert.That(result, Is.EqualTo( expected ));
+        }
+
+    }
+}
