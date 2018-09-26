d8183f13f3e2e1185aeea6ce8239e88dca3b1080
Algimantas <skuodis@gmail.com>
2018-09-26 09:24:47 +0300

    FixedCapacityStack added

---
+++ Algorithms/FixedCapacityStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class FixedCapacityStack<T>
+    {
+        private T[] StackItems;
+        private int StackPointer;
+
+        public FixedCapacityStack(int capacity)
+        {
+            this.StackItems = new T[capacity];
+            this.StackPointer = 0;
+        }
+
+        public void Push(T item)
+        {
+            this.StackItems[StackPointer++] = item;
+        }
+
+        public T Pop()
+        {
+            return this.StackItems[--this.StackPointer];
+        }
+
+        public bool IsEmpty()
+        {
+            return this.StackPointer == 0;
+        }
+
+        public int Size
+        {
+            get {
+                    return this.StackPointer;
+                }
+        }
+    }
+}
---
+++ Algorithms/FixedCapacityStackTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Algorithms
+{
+	[TestFixture]
+    public class FixedCapacityStackTest
+    {
+		[Test]
+		public void TestPushAndPop()
+        {
+            FixedCapacityStack<int> stack = new FixedCapacityStack<int>(5);
+            Assert.AreEqual(true, stack.IsEmpty());
+            stack.Push(6);
+            Assert.AreEqual(1, stack.Size);
+            stack.Push(7);
+            Assert.AreEqual(2, stack.Size);
+            stack.Push(8);
+            Assert.AreEqual(3, stack.Size);
+            stack.Pop();
+            Assert.AreEqual(2, stack.Size);
+            stack.Pop();
+            Assert.AreEqual(1, stack.Size);
+            stack.Pop();
+            Assert.AreEqual(0, stack.Size);
+        }
+    }
+}
