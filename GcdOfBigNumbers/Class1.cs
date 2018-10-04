bfa612b9fcda57589804008bac205c5d31355640
Algis <skuodis@gmail.com>
2018-10-04 19:19:21 +0300

    gdc of big numbers

---
+++ GcdOfBigNumbers/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace GcdOfBigNumbers
+{
+    public class BigNumber
+    {
+        private string numerals;
+        public BigNumber(string numerals)
+        {
+            this.numerals = numerals;
+        }
+
+        public bool IsEven()
+        {
+            return true;
+        }
+
+    }
+    public class Dividor
+    {
+        public BigNumber Divide(BigNumber a, BigNumber b)
+        {
+            int d = 0;
+            while (a.IsEven() && b.IsEven())
+            {
+                a = a.DivideByTwo();
+                b = b.DivideByTwo();
+                d++;
+
+                while (!a.Equals(b))
+                {
+                    if (a.IsEven())
+                    {
+                        a = a.DivideByTwo();
+                    } else if (b.IsEven())
+                    {
+                        b = b.DivideByTwo();
+                    } else if (a.GreaterThan(b))
+                    {
+                        a = a.Minus(b);
+                        a = a.DivideByTwo();
+                    }
+                    else
+                    {
+                        b = b.Minus(a);
+                        b = b.DivideByTwo();
+                    }
+                }
+            }
+
+            var g = a;
+            var gcd = g.MultiplyBy(2 ^ d);
+            return gcd;
+        }
+    }
+}
