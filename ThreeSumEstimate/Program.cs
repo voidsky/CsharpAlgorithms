5c9d1ccb74c8386c39b40f80b4506e09b04847d9
Algimantas <skuodis@gmail.com>
2018-10-10 13:35:42 +0300

    Three sum estimate added

---
+++ ThreeSumEstimate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeSumEstimate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(args[0]);
+            int[] a = new int[N];
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+                a[i] = rnd.Next(-100000, 100000);
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            int cnt = ThreeSum.count(a);
+            stopwatch.Stop();
+            var time = stopwatch.Elapsed;
+
+            Console.WriteLine($"{cnt} triple {time.ToString()}");
+
+        }
+    }
+}
---
+++ ThreeSumEstimate/ThreeSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeSumEstimate
+{
+    public class ThreeSum
+    {
+        public static int count(int[] a)
+        {
+            int N = a.Length;
+            int cnt = 0;
+            for (int i = 0; i < N; i++)
+                for (int j = i+1; i < N; i++)
+                    for (int k = j + 1; k < N; k++)
+                        if (a[i] + a[j] + a[k] == 0)
+                            cnt++;
+            return cnt;
+        }
+    }
+}
