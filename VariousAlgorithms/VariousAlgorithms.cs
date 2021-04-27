8b0a8f8ff60c5736e941ed52154d3e4689c4c027
Algimantas Skuodis <skuodis@gmail.com>
2021-04-27 13:40:42 +0300

    VariousAlgorithms added

---
+++ VariousAlgorithms/VariousAlgorithms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariousAlgorithms
+{
+    public static class VariousAlgorithms
+    {
+        // Havin amount of money A, how many different ways 
+        // we can change it using coinTypes ? 
+        // Structure and Interpolation of computer programs, p41
+        // This generates a tree recursive process.
+        public static int CountChange(int a, int[] coinTypes)
+        {
+            return CountChange(a, coinTypes, 0);
+        }
+        public static int CountChange(int a, int[] coinTypes, int coinIndex = 0)
+        {
+            if (a == 0) return 1;
+            if (a < 0) return 0;
+            if (coinIndex == coinTypes.Length) return 0;
+
+            return CountChange(a - coinTypes[coinIndex], coinTypes, coinIndex) +
+                CountChange(a, coinTypes, ++coinIndex);
+        }
+    }
+}
---
+++ VariousAlgorithms/VariousAlgorithmsTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariousAlgorithms
+{
+    [TestFixture]
+    public class VariousAlgorithmsTest
+    {
+        [Test]
+        [TestCase(10, new int[] { 1 }, 1)]
+        [TestCase(10, new int[] { 5 }, 1)]
+        [TestCase(5, new int[] { 5 }, 1)]
+        [TestCase(10,new int[] { 5,1}, 3)]
+        [TestCase(100, new int[] { 50, 25, 10, 5, 1 }, 292)]
+        public void CountChangeTest(int amount, int[] coinTypes, int expected)
+        {
+            int result = VariousAlgorithms.CountChange(amount, coinTypes);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
