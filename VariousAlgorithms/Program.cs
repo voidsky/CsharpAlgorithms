53ae355115f23a38e9556db6dd23a6c6a68afa53
Algimantas Skuodis <skuodis@gmail.com>
2021-04-27 14:05:38 +0300

    CountChangePerformanceTest

--- VariousAlgorithms/Program.cs
+++ VariousAlgorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,22 @@ namespace VariousAlgorithms
 {
     class Program
     {
+        static void CountChangePerformanceTest()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            for (int i = 100; i < 2000; i += 100)
+            {
+                stopwatch.Start();
+                VariousAlgorithms.CountChange(i, new int[] { 50, 25, 10, 5, 1 });
+                stopwatch.Stop();
+                Console.WriteLine($"{i} užtruko: {stopwatch.Elapsed}");
+            }
+        }
         static void Main(string[] args)
         {
+            CountChangePerformanceTest();
+
+            Console.ReadLine();
         }
     }
 }
